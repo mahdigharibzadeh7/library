6b5472163da61f9208a2574c3bec6dcbe7542ed9
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 15:36:49 +0330

    create Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div className="flex gap-x-2 mx-64 mt-20 ">
+      <input
+        type="search"
+        name="search"
+        placeholder="Search title"
+        className="rounded-lg px-2 py-2 w-72 bg-zinc-200 text-black focus:outline-none"
+      />
+      <button className="text-2xl bg-blue-700 rounded-lg p-1">🔍</button>
+    </div>
+  );
+}
+
+export default Search;
