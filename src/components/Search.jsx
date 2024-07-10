3d6bdd632ea124a391007f0a51cfb61f5c54e8b5
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 21:55:20 +0330

    set search state

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 function Search() {
+  const [searchValue, setSearchValue] = useState("");
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <div className="flex gap-x-2 mt-20 ">
+    <form className="flex gap-x-2 mt-20" onSubmit={submitHandler}>
       <input
+        className="rounded-lg px-2 py-2 w-72 bg-zinc-200 text-black focus:outline-none"
         type="search"
         name="search"
         placeholder="Search title"
-        className="rounded-lg px-2 py-2 w-72 bg-zinc-200 text-black focus:outline-none"
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
       />
-      <button className="text-2xl bg-blue-700 rounded-lg p-1">🔍</button>
-    </div>
+      <button className="text-2xl bg-blue-700 rounded-lg p-1" type="submit">
+        🔍
+      </button>
+    </form>
   );
 }
 
