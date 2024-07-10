211060daf2e2124559e5489f77dcfb9bc289ac20
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 21:54:54 +0330

    set header

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="flex justify-between items-center rounded-lg mt-3 px-5 py-3 bg-zinc-800 text-white font-medium">
+      <div className="text-3xl font-bold">Book App</div>
+      <div>
+        Designed By | <span className="text-blue-700">Mahdi Gharibzadeh</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
