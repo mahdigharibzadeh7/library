f1d217435c1e0ac6c40efbaeb0a358ee493d2611
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 23:12:43 +0330

    remove SearchBox component and move to MainCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-
 import MainCards from "./components/MainCards";
-import Search from "./components/Search";
 import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <Layout>
-      <Search />
       <MainCards />
     </Layout>
   );
