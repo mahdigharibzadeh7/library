c34681748af62ee3a2b9e2c87287cdb28f205b7f
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 21:55:28 +0330

    set footer

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="text-center rounded-lg mt-3 px-5 py-3 mt-10 mb-3 bg-zinc-800 text-white font-medium">
+      Developed By | <span className="text-blue-700">Mahdi Gharibzadeh</span>{" "}
+      with ❤️
+    </div>
+  );
+}
+
+export default Footer;
