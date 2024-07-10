abf07a7eb9fac553774c645fb4bc9af9b0a7c7a7
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 16:26:39 +0330

    add BookList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Search from "./components/Search";
+import BooksList from "./components/BooksList"
 
 function App() {
   return (
     <div>
       <Header />
       <Search />
+      <BooksList />
     </div>
   );
 }
