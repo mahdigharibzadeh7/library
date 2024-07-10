c6c31f3cf8f1d90b0f165cab9a4ce5212fa76ec0
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 23:13:32 +0330

    add search state and function and book state

--- src/components/MainCards.jsx
+++ src/components/MainCards.jsx
@@ -1,14 +1,44 @@
 import { useState } from "react";
+
 import BooksList from "./BooksList";
 import FavoritesList from "./FavoritesList";
+import SearchBox from "./SearchBox";
+
+import { books as bookData } from "../constants/books";
 
 function MainCards() {
   const [faveBooks, setFaveBooks] = useState([]);
+  const [search, setSearch] = useState("");
+  const [books, setBooks] = useState(bookData);
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    if (search) {
+      const newBooks = bookData.filter((book) =>
+        book.title.toLowerCase().includes(search)
+      );
+      setBooks(newBooks);
+    } else {
+      setBooks(bookData);
+    }
+  };
+
   return (
-    <div className="flex mt-7">
-      <BooksList faveBooks={faveBooks} setFaveBooks={setFaveBooks} />
-      {!!faveBooks.length && <FavoritesList faveBooks={faveBooks} />}
-    </div>
+    <>
+      <SearchBox
+        search={search}
+        setSearch={setSearch}
+        searchHandler={searchHandler}
+      />
+      <div className="flex mt-7">
+        <BooksList
+          faveBooks={faveBooks}
+          setFaveBooks={setFaveBooks}
+          books={books}
+        />
+        {!!faveBooks.length && <FavoritesList faveBooks={faveBooks} />}
+      </div>
+    </>
   );
 }
 
