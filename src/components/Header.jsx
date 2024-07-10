cdff17fde185bb9f5ea503a3271ed0a6e23ee440
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 15:25:24 +0330

    create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-between items-center rounded-lg mx-64 mt-3 px-5 py-3 bg-zinc-800 text-white font-medium">
+      <div className="text-3xl font-bold">Book App</div>
+      <div>
+        Designed By | <span className="text-blue-700">Mahdi Gharibzadeh</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
