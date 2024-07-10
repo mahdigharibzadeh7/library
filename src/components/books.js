f691bc7f4262a498f3ada89cbdff73a7d715b2a0
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-10 16:31:21 +0330

    create books constant

---
+++ src/components/books.js
@@ -0,0 +1,46 @@
+const books = [
+  {
+    id: 1,
+    name: "book1",
+    author: "author1",
+    lang: "lang1",
+    numOfPages: "100",
+  },
+  {
+    id: 2,
+    name: "book2",
+    author: "author2",
+    lang: "lang2",
+    numOfPages: "200",
+  },
+  {
+    id: 3,
+    name: "book3",
+    author: "author3",
+    lang: "lang3",
+    numOfPages: "300",
+  },
+  {
+    id: 4,
+    name: "book4",
+    author: "author4",
+    lang: "lang4",
+    numOfPages: "400",
+  },
+  {
+    id: 5,
+    name: "book5",
+    author: "author5",
+    lang: "lang5",
+    numOfPages: "500",
+  },
+  {
+    id: 6,
+    name: "book6",
+    author: "author6",
+    lang: "lang6",
+    numOfPages: "600",
+  },
+];
+
+export default books;
